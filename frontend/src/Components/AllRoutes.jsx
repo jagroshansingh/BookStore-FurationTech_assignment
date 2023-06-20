2fbfc82f28465fd1f51481f4a820572aa34d08d0
Jagroshan <jagroshansingh615@gmail.com>
2023-06-20 22:03:13 +0530

    initialised redux store with react-router and chakra-ui library

---
+++ frontend/src/Components/AllRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+export const AllRoutes = () => {
+  return (
+    <div>
+        <Routes>
+            {/* <Route path='/' /> */}
+        </Routes>
+    </div>
+  )
+}
---
+++ frontend/src/Redux/reducer.js
@@ -0,0 +1,9 @@
+const initial={
+ book:null
+}
+
+export const reducer=(state=initial,{type,payload})=>{
+    switch(type){
+        default:return state
+    }
+}
