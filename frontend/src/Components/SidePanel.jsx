3760868358e14ce76526eb1f4fb7c1a502992368
Jagroshan <jagroshansingh615@gmail.com>
2023-06-21 15:55:14 +0530

    URL params made dynamic for filter action

--- frontend/src/Components/SidePanel.jsx
+++ frontend/src/Components/SidePanel.jsx
@@ -1,35 +1,101 @@
-import { Checkbox, Heading } from '@chakra-ui/react'
-import React from 'react'
-import styles from './CSS/SidePanel.module.css'
+import { Checkbox, Heading } from "@chakra-ui/react";
+import React from "react";
+import styles from "./CSS/SidePanel.module.css";
+import { useSearchParams } from "react-router-dom";
 
 export const SidePanel = () => {
-  const handleGenre=(e)=>{
-    console.log(e.target.checked)
-    console.log(e.target.value)
-  }
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const handleEdition=(e)=>{
-    console.log(e.target.value)
-  }
+  const initialGenre = searchParams.getAll("genre");
+  const [genre, setGenre] = React.useState(initialGenre || []);
+
+  const initialEdition = searchParams.getAll("edition");
+  const [edition, setEdition] = React.useState(initialEdition || []);
+
+  const handleGenre = (e) => {
+    if (e.target.checked) setGenre([...genre, e.target.value]);
+    else {
+      let updateGenre = [...genre];
+      updateGenre.splice(updateGenre.indexOf(e.target.value), 1);
+      setGenre(updateGenre);
+    }
+  };
+
+  const handleEdition = (e) => {
+    if (e.target.checked) setEdition([...edition, e.target.value]);
+    else {
+      let updateEdition = [...edition];
+      updateEdition.splice(updateEdition.indexOf(e.target.value), 1);
+      setEdition(updateEdition);
+    }
+  };
+
+  React.useEffect(() => {
+    const params = {
+      genre,
+      edition,
+    };
+    setSearchParams(params);
+  }, [genre, edition]);
   return (
     <div>
       <Heading>Filter</Heading>
       <div className={styles.filterContainer}>
         <div>
-        <Heading>Genre</Heading>
-        <Checkbox value={'Science'} onChange={handleGenre}>Science</Checkbox>
-        <Checkbox value={'Fiction'} onChange={handleGenre}>Fiction</Checkbox>
-        <Checkbox value={'History'} onChange={handleGenre}>History</Checkbox>
-        <Checkbox value={'Tech'} onChange={handleGenre}>Tech</Checkbox>
-        <Checkbox value={'Business'} onChange={handleGenre}>Business</Checkbox>
+          <Heading>Genre</Heading>
+          <div>
+          <Checkbox
+            defaultChecked={genre.includes("Science")}
+            value={"Science"}
+            onChange={handleGenre}
+          >
+            Science
+          </Checkbox>
+          <Checkbox
+            defaultChecked={genre.includes("Fiction")}
+            value={"Fiction"}
+            onChange={handleGenre}
+          >
+            Fiction
+          </Checkbox>
+          <Checkbox
+            defaultChecked={genre.includes("History")}
+            value={"History"}
+            onChange={handleGenre}
+          >
+            History
+          </Checkbox>
+          <Checkbox
+            defaultChecked={genre.includes("Tech")}
+            value={"Tech"}
+            onChange={handleGenre}
+          >
+            Tech
+          </Checkbox>
+          <Checkbox
+            defaultChecked={genre.includes("Business")}
+            value={"Business"}
+            onChange={handleGenre}
+          >
+            Business
+          </Checkbox>
+          </div>
         </div>
         <div>
-        <Heading>Edition</Heading>
-        <Checkbox value={'2021'} onChange={handleEdition}>2021</Checkbox>
-        <Checkbox value={'2022'} onChange={handleEdition}>2022</Checkbox>
-        <Checkbox value={'2023'} onChange={handleEdition}>2023</Checkbox>
+          <Heading>Edition</Heading>
+          <div>
+          <Checkbox defaultChecked={edition.includes("2021")} value={"2021"} onChange={handleEdition}>
+            2021
+          </Checkbox>
+          <Checkbox defaultChecked={edition.includes("2022")} value={"2022"} onChange={handleEdition}>
+            2022
+          </Checkbox>
+          <Checkbox defaultChecked={edition.includes("2023")} value={"2023"} onChange={handleEdition}>
+            2023
+          </Checkbox>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
