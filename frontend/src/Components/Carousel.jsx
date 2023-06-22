3026e81edc58b4f142c8359432cc9be6f3733c75
Jagroshan <jagroshansingh615@gmail.com>
2023-06-23 00:17:48 +0530

    Carousel is made working

---
+++ frontend/src/Components/Carousel.jsx
@@ -0,0 +1,48 @@
+import { Flex, IconButton, Image } from "@chakra-ui/react";
+import React from "react";
+import {ChevronLeftIcon, ChevronRightIcon} from '@chakra-ui/icons'
+import { useNavigate } from "react-router-dom";
+import styles from "./CSS/Carousel.module.css"
+
+export const Carousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+  const navigate=useNavigate()
+
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 4 : prevIndex - 1
+    );
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 4 ? 0 : prevIndex + 1
+    );
+  };
+  return (
+    <div>
+      <Flex align="center" justify="space-between">
+        <IconButton
+          icon={<ChevronLeftIcon />}
+          onClick={handlePrevClick}
+          disabled={currentIndex === 0}
+        />
+        <Flex className={styles.imageContainer}>
+          {images.slice(currentIndex, currentIndex + 4).map((image, index) => (
+            <Image
+              key={index}
+              src={image}
+              alt={`Image ${index}`}
+              onClick={()=>navigate('/booklisting')}
+            />
+          ))}
+        </Flex>
+        <IconButton
+          icon={<ChevronRightIcon />}
+          onClick={handleNextClick}
+          disabled={currentIndex === images.length - 4}
+        />
+      </Flex>
+    </div>
+  );
+};
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import { Navbar } from '../Components/Navbar'
+import React from "react";
+import { Navbar } from "../Components/Navbar";
+import { Carousel } from "../Components/Carousel";
+import styles from "./CSS/Home.module.css"
 
 export const Home = () => {
+  const images = [
+    "https://m.media-amazon.com/images/I/91bYsX41DVL._AC_UY327_FMwebp_QL65_.jpg",
+    "https://m.media-amazon.com/images/I/71W5IMP9-cL._AC_UL480_FMwebp_QL65_.jpg",
+    "https://m.media-amazon.com/images/I/71vxH0htYvL._AC_UL480_FMwebp_QL65_.jpg",
+    "https://m.media-amazon.com/images/I/71QH6ZsXHKL._AC_UL480_FMwebp_QL65_.jpg",
+    "https://m.media-amazon.com/images/I/71sBtM3Yi5L._AC_UY327_FMwebp_QL65_.jpg",
+    "https://m.media-amazon.com/images/I/71B4h-dSVzL._AC_UY327_FMwebp_QL65_.jpg",
+  ];
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <div className={styles.carousel}>
+        <Carousel images={images} />
+      </div>
     </div>
-  )
-}
+  );
+};
